f81b600e77213fb005f3d31ed366af96023e9ea3
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-07-20 07:57:23 +0300

    main presets

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((err, _, res, next) => {
+  const { status = 500 } = err;
+  res.status(status).json({ message: err.message });
 });
 
 module.exports = app;
